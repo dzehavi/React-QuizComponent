187a42a2540b420274e8fe5a845e7397c5021e83
Doron Zehavi <Doron_Zehavi@amat.com>
2018-06-19 17:05:52 +0300

    Module 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
     this.state = {quiz_position: 1}
   }
 
+  handleResetClick() {
+    this.setState((state) =>{
+      return {quiz_position: 1}
+    })
+  }
+
   showNextQuestion() {
     this.setState((state) => {
       return {quiz_position: state.quiz_position + 1}
@@ -22,7 +28,7 @@ class Quiz extends Component {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
     return (
       <div>
-        {isQuizEnd && <QuizEnd />}
+        {isQuizEnd && <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>}
         {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
       </div>
     )
